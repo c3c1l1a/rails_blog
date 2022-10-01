da4412d3e23bfe45280b967df2afbf3118ae8781
c3c1l1a <cesswangumukima@gmail.com>
2022-10-01 11:47:42 +0800

    Fix linter errors

--- spec/comment_spec.rb
+++ spec/comment_spec.rb
@@ -1,31 +1,31 @@
 require 'rails_helper'
 describe Comment, type: :model do
-  let (:author) { 
-    User.create(name: 'Lilly', 
-        photo: 'https://unsplash.com/photos/F_-0BxGuVvo', 
-        bio: 'Teacher from Poland',
-        posts_counter: 0
-      )}
+  let(:author) do
+    User.create(name: 'Lilly',
+                photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
+                bio: 'Teacher from Poland',
+                posts_counter: 0)
+  end
 
-  let (:post) {
+  let(:post) do
     Post.create(
       author: author,
       title: 'Hello',
       text: 'This is my first post',
       comments_counter: 0,
       likes_counter: 0
-    )}
+    )
+  end
 
-  subject {
+  subject do
     Comment.create(post: post, author: author, text: 'This is my comment')
-  }
+  end
 
   it 'checkes if like is valid' do
     expect(subject).to be_valid
   end
 
-  it 'updates likes counter for a post' do 
-    expect{ subject.update_comments_counter_for_post }.to change { post.comments_counter }.by(1)
+  it 'updates likes counter for a post' do
+    expect { subject.update_comments_counter_for_post }.to change { post.comments_counter }.by(1)
   end
-
 end
--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -1,16 +1,16 @@
 require 'rails_helper'
 describe User, type: :model do
-  subject {
-    User.create(name: 'Lilly', 
-      photo: 'https://unsplash.com/photos/F_-0BxGuVvo', 
-      bio: 'Teacher from Poland')
-  }
+  subject do
+    User.create(name: 'Lilly',
+                photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
+                bio: 'Teacher from Poland')
+  end
 
-  before (:each) do 
-    3.times { 
-      post = Post.new(author: subject, title: 'Intro', text: 'Welcome to this blog!') 
+  before(:each) do
+    3.times do
+      post = Post.new(author: subject, title: 'Intro', text: 'Welcome to this blog!')
       post.update_posts_counter_for_user
-    } 
+    end
   end
 
   it 'invalidate when name is not given' do
@@ -18,21 +18,21 @@ describe User, type: :model do
     expect(subject).to_not be_valid
   end
 
-  it 'validate when name is given' do 
+  it 'validate when name is given' do
     expect(subject).to be_valid
   end
 
-  it 'validates post counter is > 0' do 
+  it 'validates post counter is > 0' do
     subject.posts_counter = -1
     expect(subject).to_not be_valid
   end
 
-  it 'validates post counter in an integer' do 
+  it 'validates post counter in an integer' do
     subject.posts_counter = 1.1
     expect(subject).to_not be_valid
   end
 
-  it 'Fetches 3 recent posts' do 
+  it 'Fetches 3 recent posts' do
     expect(subject.recent_posts.size).to eql(subject.posts.last(3).size)
   end
 end
