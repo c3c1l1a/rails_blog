5a3c690c0873055d50b12b28b1d08a9dca244350
c3c1l1a <cesswangumukima@gmail.com>
2022-10-06 16:48:40 +0800

    Remove linter errors

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -1,34 +1,33 @@
-require "rails_helper"
+require 'rails_helper'
 
-RSpec.describe "User controller", :type => :request do
-  it "checks if response status code 200" do
-    get "/users/1/posts"
+RSpec.describe 'User controller', type: :request do
+  it 'checks if response status code 200' do
+    get '/users/1/posts'
     expect(response).to have_http_status(200)
   end
 
-  it "checks if index template was rendered." do
-    get "/users/1/posts"
+  it 'checks if index template was rendered.' do
+    get '/users/1/posts'
     expect(response).to render_template(:index)
   end
 
-  it "checks if the response body includes correct placeholder text." do 
-    get "/users/1/posts"
-    expect(response.body).to include("List all posts for a particular user here ")
+  it 'checks if the response body includes correct placeholder text.' do
+    get '/users/1/posts'
+    expect(response.body).to include('List all posts for a particular user here ')
   end
 
-  it "checks if response status code 200 for a single post" do 
-    get "/users/1/posts/1"
+  it 'checks if response status code 200 for a single post' do
+    get '/users/1/posts/1'
     expect(response).to have_http_status(200)
   end
 
-  it "checks if show template was rendered for a single post." do
-    get "/users/1/posts/1"
+  it 'checks if show template was rendered for a single post.' do
+    get '/users/1/posts/1'
     expect(response).to render_template(:show)
   end
 
-  it "checks if the response body includes correct placeholder text for a single user." do 
-    get "/users/1/posts/1"
-    expect(response.body).to include("Information for a particular post goes here")
+  it 'checks if the response body includes correct placeholder text for a single user.' do
+    get '/users/1/posts/1'
+    expect(response.body).to include('Information for a particular post goes here')
   end
-
 end
