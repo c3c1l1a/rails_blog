9ef04876161e35734818a5724f655acc2fc29f7b
c3c1l1a <cesswangumukima@gmail.com>
2022-10-07 02:38:25 +0800

    Update the spec to check if view has correct body test

--- spec/controllers/user_controller_spec.rb
+++ spec/controllers/user_controller_spec.rb
@@ -1,15 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe 'User controller', type: :request do
-  before(:each) do
-    @user = User.create!(name: 'Tom',
-                         photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
-                         bio: 'Teacher from Mexico.',
-                         posts_counter: 0)
+  before (:each) do 
+    @user = User.create!(name: 'Tom', 
+      photo: 'https://unsplash.com/photos/F_-0BxGuVvo', 
+      bio: 'Teacher from Mexico.', 
+      posts_counter: 0)
   end
   it 'checks if response status code 200' do
     get '/users'
     expect(response).to have_http_status(200)
+  end
+
+  it 'checks if the response body includes correct placeholder text.' do
+    get '/users'
+    expect(response.body).to include('Tom')
   end
 
   it 'checks if index template was rendered.' do
@@ -20,6 +25,11 @@ RSpec.describe 'User controller', type: :request do
   it 'checks if response status code 200 for a single user' do
     get "/users/#{@user.id}"
     expect(response).to have_http_status(200)
+  end
+
+  it 'checks if the response body includes correct placeholder text.' do
+    get "/users/#{@user.id}"
+    expect(response.body).to include('Teacher from Mexico.')
   end
 
   it 'checks if show template was rendered for a single user.' do
