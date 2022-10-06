f613fac66a4f5ebd35b40d80921e4429ebdc970d
c3c1l1a <cesswangumukima@gmail.com>
2022-10-06 22:03:26 +0800

    Add view for a particular user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,8 @@ class UsersController < ApplicationController
   def index
     @users = User.all
   end
-
-  def show; end
+  def show
+    @user = User.find params[:id]
+    @posts = Post.where(author_id: @user)
+  end
 end
