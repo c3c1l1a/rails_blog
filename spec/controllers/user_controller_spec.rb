ecc6c0bc82e73696d1d9e506331d4c056230ce0a
c3c1l1a <cesswangumukima@gmail.com>
2022-10-07 02:29:09 +0800

    Update user spec for a particular user

--- spec/controllers/user_controller_spec.rb
+++ spec/controllers/user_controller_spec.rb
@@ -1,6 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe 'User controller', type: :request do
+  before (:each) do 
+    @user = User.create!(name: 'Tom', 
+      photo: 'https://unsplash.com/photos/F_-0BxGuVvo', 
+      bio: 'Teacher from Mexico.', 
+      posts_counter: 0)
+  end
   it 'checks if response status code 200' do
     get '/users'
     expect(response).to have_http_status(200)
@@ -12,22 +18,12 @@ RSpec.describe 'User controller', type: :request do
   end
 
   it 'checks if response status code 200 for a single user' do
-    user = User.create!(name: 'Tom', 
-      photo: 'https://unsplash.com/photos/F_-0BxGuVvo', 
-      bio: 'Teacher from Mexico.', 
-      posts_counter: 0)
-
-    get "/users/#{user.id}"
+    get "/users/#{@user.id}"
     expect(response).to have_http_status(200)
   end
 
   it 'checks if show template was rendered for a single user.' do
-    user = User.create!(name: 'Tom', 
-      photo: 'https://unsplash.com/photos/F_-0BxGuVvo', 
-      bio: 'Teacher from Mexico.', 
-      posts_counter: 0)
-
-    get "/users/#{user.id}"
+    get "/users/#{@user.id}"
     expect(response).to render_template(:show)
   end
 end
