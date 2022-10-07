458f517a2aa0d0926bb0eb699b9371714e3f3421
c3c1l1a <cesswangumukima@gmail.com>
2022-10-07 14:22:03 +0800

    Update spec for the show controller

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -28,18 +28,18 @@ RSpec.describe 'Post controller', type: :request do
     expect(response.body).to include('This is my first post')
   end
 
-  # it 'checks if response status code 200 for a single post' do
-  #   get '/users/1/posts/1'
-  #   expect(response).to have_http_status(200)
-  # end
+  it 'checks if response status code 200 for a single post' do
+    get "/users/#{@user.id}/posts/#{@post.id}"
+    expect(response).to have_http_status(200)
+  end
 
-  # it 'checks if show template was rendered for a single post.' do
-  #   get '/users/1/posts/1'
-  #   expect(response).to render_template(:show)
-  # end
+  it 'checks if show template was rendered for a single post.' do
+    get "/users/#{@user.id}/posts/#{@post.id}"
+    expect(response).to render_template(:show)
+  end
 
-  # it 'checks if the response body includes correct placeholder text for a single user.' do
-  #   get '/users/1/posts/1'
-  #   expect(response.body).to include('Information for a particular post goes here')
-  # end
+  it 'checks if the response body includes correct placeholder text for a single user.' do
+    get "/users/#{@user.id}/posts/#{@post.id}"
+    expect(response.body).to include('This is my first post')
+  end
 end
