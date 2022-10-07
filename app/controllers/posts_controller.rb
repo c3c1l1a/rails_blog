4311e6751fd9bce549bf6a63c1e1bc9d3c2e1043
c3c1l1a <cesswangumukima@gmail.com>
2022-10-07 13:09:13 +0800

    Add comments for post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,18 @@ class PostsController < ApplicationController
   def index
     @user = User.find params[:user_id]
     @posts = Post.where(author_id: params[:user_id])
+    @comments = Comment.all 
+
+    @comments_info = []
+    @comments.each do |comment|
+      pp comment
+      user = User.find comment.author_id
+       @comments_info << { 
+          user_name: user.name,
+          text: comment.text,
+          post_id: comment.post_id
+       }
+    end
   end
   def show; end
 end
