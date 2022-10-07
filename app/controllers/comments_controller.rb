eb8fc986baf9f0710f99104a2a5719f57dc66fa6
c3c1l1a <cesswangumukima@gmail.com>
2022-10-08 02:02:30 +0800

    Add create action for comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,15 @@
 class CommentsController < ApplicationController
   def new
   end
+
+  def create 
+    data = params.require(:comment).permit( :text)
+    post = Post.find(params[:post_id])
+    @comment = Comment.create!(
+                        post: post,
+                        author: current_user, 
+                        text: data[:text])
+    redirect_to user_posts_path
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   # root "articles#index"
   resources :users, only: %i[index show] do
     resources :posts, only: %i[index show new create] do 
-      resources :comments, only: %i[new]
+      resources :comments, only: %i[new create]
     end
   end
 end
