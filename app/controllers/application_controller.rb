4bcc3b96b5cbfe541b6b1713d6d94463049dadde
c3c1l1a <cesswangumukima@gmail.com>
2022-10-20 00:51:53 +0800

    Add more fields to the signup form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+	before_action :update_allowed_parameters, if: :devise_controller?
+
+	protected
+
+ def update_allowed_parameters
+ 		devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :photo, :bio])
+ end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, path: '/', path_names: {
     sign_in: 'login'
   }
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
