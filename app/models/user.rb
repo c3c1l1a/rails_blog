a09cfe454f8464ab06103a45b0c559ef57943396
c3c1l1a <cesswangumukima@gmail.com>
2022-10-20 02:13:33 +0800

    Generated devise controllers for users and cofigured it

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
   has_many :likes, foreign_key: :like_id
 
   validates :name, presence: true
-  validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0, allow_nil:true }
 
   def recent_posts
     posts.order('created_at Desc').limit(3)
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, path: '/', path_names: {
     sign_in: 'login'
-  }
+  }, controllers: { sessions: 'users/sessions' }
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
