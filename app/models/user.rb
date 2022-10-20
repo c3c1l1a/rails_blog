9d3c0b56fd32755a13fe6cf76ccf20196ac00040
c3c1l1a <cesswangumukima@gmail.com>
2022-10-20 19:56:20 +0800

    Make signup confirmable

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :validatable 
   has_many :comments, foreign_key: :comment_id
   has_many :posts, foreign_key: :post_id
