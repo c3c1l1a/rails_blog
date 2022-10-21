9f8e1921b7f738d798fadbb4e6644ff01e4df570
c3c1l1a <cesswangumukima@gmail.com>
2022-10-21 14:10:08 +0800

    Add test for profile picture

--- spec/views/user_index_spec.rb
+++ spec/views/user_index_spec.rb
@@ -2,16 +2,27 @@ require 'rails_helper'
 
 RSpec.describe 'User index', type: :system do
   before(:each) do
-    @user = User.create!(name: 'Tom',
+    @user1 = User.create!(name: 'Tom',
                          photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
                          bio: 'Teacher from Mexico.',
                          posts_counter: 0)
   end
-  
-  it 'shows the right content' do
+
+  it 'can see username of all other users' do
     visit users_path
     sleep(5)
-    expect(page).to have_content('')
+    expect(page).to have_content('Tom')
   end
+
+  it 'can seee profile picture for each user' do
+    visit users_path
+    sleep(5)
+    image=find(:xpath, "//img[@class='user-image']")
+    image['src'].should eq('https://unsplash.com/photos/F_-0BxGuVvo/')
+  end
+
+
+
+
   
 end
