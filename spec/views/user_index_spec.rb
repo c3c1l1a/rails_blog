4e050ecc43a5e76d42a090da96cb089f0318919a
c3c1l1a <cesswangumukima@gmail.com>
2022-10-21 14:12:14 +0800

    Add test for posts counter

--- spec/views/user_index_spec.rb
+++ spec/views/user_index_spec.rb
@@ -21,6 +21,11 @@ RSpec.describe 'User index', type: :system do
     image['src'].should eq('https://unsplash.com/photos/F_-0BxGuVvo/')
   end
 
+  it 'can see the number of posts each user has written.' do
+    visit users_path
+    sleep(5)
+    expect(page).to have_content(@user1.posts_counter)
+  end
 
 
 
