30dcb4d5faf0438f9bf17af378c3dd0142295ab2
c3c1l1a <cesswangumukima@gmail.com>
2022-10-21 14:49:20 +0800

    can see the user profile picture on the show page

---
+++ spec/views/user_show_spec.rb
@@ -0,0 +1,43 @@
+require 'rails_helper'
+
+RSpec.describe 'User show', type: :system do
+  before(:each) do
+    @user1 = User.create!(name: 'Tom',
+                         photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
+                         bio: 'Teacher from Mexico.',
+                         posts_counter: 0)
+  end
+
+  it 'can see the users profile picture' do
+    visit users_path
+    sleep(5)
+    click_link @user1.name
+    sleep(5)
+    image=find(:xpath, "//img[@class='user-image']")
+    image['src'].should eq('https://unsplash.com/photos/F_-0BxGuVvo/')
+  end
+
+  # it 'can seee profile picture for each user' do
+  #   visit users_path
+  #   sleep(5)
+  #   image=find(:xpath, "//img[@class='user-image']")
+  #   image['src'].should eq('https://unsplash.com/photos/F_-0BxGuVvo/')
+  # end
+
+  # it 'can see the number of posts each user has written.' do
+  #   visit users_path
+  #   sleep(5)
+  #   expect(page).to have_content(@user1.posts_counter)
+  # end
+
+  # it 'When I click on a user, I am redirected to that users show page' do
+  #   visit users_path
+  #   sleep(5)
+  #   click_link @user1.name
+  #   expect(page).to have_content(@user1.bio)
+  # end
+
+
+
+  
+end
