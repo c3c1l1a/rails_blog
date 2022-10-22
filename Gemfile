7b7131291332db145ee71b19bf81d2fbc4482493
petushka1 <nadya@devery.io>
2022-10-22 22:03:54 +0300

    add documentation

--- Gemfile
+++ Gemfile
@@ -29,6 +29,9 @@ gem 'jbuilder'
 # Use Redis adapter to run Action Cable in production
 # gem "redis", "~> 4.0"
 
+# use rswag to generate api documentation https://github.com/rswag/rswag
+gem 'rswag'
+
 # Use Kredis to get higher-level data types in Redis [https://github.com/rails/kredis]
 # gem "kredis"
 
@@ -48,6 +51,8 @@ gem 'bootsnap', require: false
 # gem "image_processing", "~> 1.2"
 
 gem 'devise'
+
+gem 'ffi'
 
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
---
+++ spec/requests/api/comments_spec.rb
@@ -0,0 +1,48 @@
+require 'swagger_helper'
+
+RSpec.describe 'api/v1/comments', type: :request do
+
+  path '/api/v1/users/{user_id}/posts/{post_id}/comments' do
+
+    parameter name: 'user_id', in: :path, type: :integer, description: 'Author Id'
+    parameter name: 'post_id', in: :path, type: :integer, description: 'Post Id'
+
+    get('list comments') do
+      produces 'application/json'
+
+      response(200, '') do
+        schema '$ref' => '#/components/schemas/comments'
+
+        run_test!
+      end
+
+      response(404, 'comments not found') { run_test! }
+    end
+  end
+
+  path '/api/v1/posts/{post_id}/comments' do
+    parameter name: 'post_id', in: :path, type: :integer, description: 'Post Id'
+
+    post('create comment') do
+      consumes 'application/json'
+      produces 'application/json'
+
+      parameter name: :comment, in: :body, schema: { '$ref' => '#/components/schemas/comment' }
+      security [{ bearerAuth: [] }]
+
+      response(201, 'created successfully!') do
+        schema '$ref' => '#/components/schemas/comment_failed'
+
+        run_test!
+      end
+
+      response(401, 'unauthorized') { run_test! }
+
+      response(:unprocessable_entity, 'Error') do
+        schema '$ref' => '#/components/schemas/comment_failed'
+
+        run_test!
+      end
+    end
+  end
+end
